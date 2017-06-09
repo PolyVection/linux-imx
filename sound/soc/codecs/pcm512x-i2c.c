da00b4085ffc5ed5b095ba4795d544a4d589e045
PolyVection <info@polyvection.com>
2017-06-09 12:22:45 +0200

    added VoltaStream ZERO

--- sound/soc/codecs/pcm512x-i2c.c
+++ sound/soc/codecs/pcm512x-i2c.c
@@ -48,6 +48,7 @@ static const struct i2c_device_id pcm512x_i2c_id[] = {
 	{ "pcm5122", },
 	{ "pcm5141", },
 	{ "pcm5142", },
+	{ "pcm5242", },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, pcm512x_i2c_id);
@@ -57,6 +58,7 @@ static const struct of_device_id pcm512x_of_match[] = {
 	{ .compatible = "ti,pcm5122", },
 	{ .compatible = "ti,pcm5141", },
 	{ .compatible = "ti,pcm5142", },
+	{ .compatible = "ti,pcm5242", },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, pcm512x_of_match);
